fe3b9d7502db134f31e9761676e0c38a38b50fe0
Jennifer Brabec <jenniferbrabec@gmail.com>
2016-10-02 05:41:06 +0000

    tax_calculation and is_berry functions in assessment.py work

--- assessment.py
+++ assessment.py
@@ -12,19 +12,39 @@
 #    Your function should return the total cost of the item,
 #    including tax.
 
-#    If the user does not provide a tax rate it should default to 5% 
+#    If the user does not provide a tax rate it should default to 5%
+
+
+def tax_calculation(cost, state, tax=0.05):
+    if state.upper() == "CA":
+        return cost + cost * 0.07
+    else:
+        return cost + cost * tax
+
 
 #####################################################################
 # PART TWO
 
 # 1. (a) Write a function, `is_berry()`, which takes a fruit name as a string
-#        and returns a boolean if the fruit is a "strawberry", "cherry", or 
+#        and returns a boolean if the fruit is a "strawberry", "cherry", or
 #        "blackberry".
 
+
+def is_berry(fruit):
+    valid_fruits = ["strawberry", "cherry", "blackberry"]
+    if fruit.lower() in valid_fruits:
+        return True
+    else:
+        return False
+
 #    (b) Write another function, shipping_cost(), which calculates shipping cost
-#        by taking a fruit name as a string, calling the `is_berry()` function 
+#        by taking a fruit name as a string, calling the `is_berry()` function
 #        within the `shipping_cost()` function and returns `0` if ``is_berry()
 #        == True``, and `5` if ``is_berry() == False``.
+
+
+# def shipping_cost():
+    
 
 # 2. (a) Write a function, `is_hometown()`, which takes a town name as a string
 #        and evaluates to `True` if it is your hometown, and `False` otherwise.
@@ -36,18 +56,18 @@
 #    (c) Write a function, `hometown_greeting()`, which takes a home town, a
 #        first name, and a last name as strings as arguments, calls both
 #        `is_hometown()` and `full_name()` and prints "Hi, 'full name here',
-#        we're from the same place!", or "Hi 'full name here', where are you 
+#        we're from the same place!", or "Hi 'full name here', where are you
 #        from?" depending on what `is_hometown()` evaluates to.
 
 #####################################################################
 
 # PART THREE
 
-# 1. Write a function ``increment()`` with a nested inner function, ``add()`` 
+# 1. Write a function ``increment()`` with a nested inner function, ``add()``
 #    inside of it. The outer function should take ``x``, an integer which
 #    defaults to 1. The inner function should take ``y`` and add ``x`` and ``y`` together.
 
-# 2. Call the function ``increment()`` with x = 5. Assign what is returned to a variable name, addfive. Call 
+# 2. Call the function ``increment()`` with x = 5. Assign what is returned to a variable name, addfive. Call
 #    addfive with y = 5. Call again with y = 20.
 
 # 3. Make a function that takes in a number and a list of numbers. It should append
